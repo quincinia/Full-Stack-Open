ee1f55f7cc72d906a161cb941126f9cddfeadfcb
quincinia <eanj13@gmail.com>
2021-06-04 10:46:54 -0700

    finished exercises 1.1 and 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,19 +11,42 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={[
+        {name: part1, exercises: exercises1},
+        {name: part2, exercises: exercises2},
+        {name: part3, exercises: exercises3}
+      ]} />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
 }
 
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+  <div>
+    {
+      props.parts.map((item, index) => (
+        <Part 
+          name={item.name} 
+          exercises={item.exercises} 
+          key={index}
+        />
+      ))
+    }
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.name} {props.exercises}
+  </p>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercises.reduce((acc, item) => acc + item, 0)}</p>
+)
 export default App
