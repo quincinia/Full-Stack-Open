b6dee9a47fda1b92e54683a18d326df68e27917d
quincinia <eanj13@gmail.com>
2021-06-07 10:19:38 -0700

    finished 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +16,21 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      {/* actually not needed bc buttons stack horizontally */}
+      <div id="button-container"> 
+        <Button text="good" handleClick={() => setGood(good + 1)}/>
+        <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}/>
+        <Button text="bad" handleClick={() => setBad(bad + 1)}/>
+      </div>
+      <h1>statistics</h1>
+      <div>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+      </div>
     </div>
   )
 }
