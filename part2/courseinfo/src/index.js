4aff71cc2eed2a6215479cfddeae2aaf44ffb71f
quincinia <eanj13@gmail.com>
2021-06-08 10:13:30 -0700

    finished up to 2.3

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -6,11 +6,12 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-    const sum =
-        course.parts[0].exercises +
-        course.parts[1].exercises +
-        course.parts[2].exercises
-    return <p>Number of exercises {sum}</p>
+    const sum = course.parts.reduce((sum, item) => sum + item.exercises, 0)
+    return (
+        <p>
+            <b>total of {sum} exercises</b>
+        </p>
+    )
 }
 
 const Part = (props) => {
@@ -63,6 +64,11 @@ const App = () => {
                 name: "State of a component",
                 exercises: 14,
                 id: 3,
+            },
+            {
+                name: "Redux",
+                exercises: 11,
+                id: 4,
             },
         ],
     }
