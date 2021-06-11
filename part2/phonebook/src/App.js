e863aed49d17cc21d3bac1f14fe3a39dffe67d5d
quincinia <eanj13@gmail.com>
2021-06-11 12:16:35 -0700

    finished 2.11; set up database and rendered info from it

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Filter = (props) => {
     return (
@@ -40,15 +41,20 @@ const Persons = (props) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456" },
-        { name: "Ada Lovelace", number: "39-44-5323523" },
-        { name: "Dan Abramov", number: "12-43-234345" },
-        { name: "Mary Poppendieck", number: "39-23-6423122" },
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState("")
     const [newNumber, setNewNumber] = useState("")
     const [nameFilter, setNameFilter] = useState("")
+
+    const hook = () => {
+        console.log("Grabbing data from server")
+        axios.get("http://localhost:3001/persons").then((response) => {
+            console.log("data: ", response)
+            setPersons(response.data)
+        })
+    }
+
+    useEffect(hook, [])
 
     const handleNameChange = (event) => {
         console.log(event.target.value)
