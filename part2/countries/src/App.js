2a1e22c7e9a2f44cfacb656a091da3cc791a5a61
quincinia <eanj13@gmail.com>
2021-06-12 10:42:02 -0700

    finished 2.12, couldn't get image resizing to work

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SizeError = (props) => {
+    return <div>Too many matches, specify another filter</div>
 }
 
-export default App;
+const CountryInfo = (props) => {
+    let { name, capital, population, languages, flag } = props
+    return (
+        <div id="country-info">
+            <h1>{name}</h1>
+            capital {capital}
+            <br />
+            population {population}
+            <h2>languages</h2>
+            <ul>
+                {languages.map((language, index) => (
+                    <li key={index}>{language}</li>
+                ))}
+            </ul>
+            <img src={flag} alt={`flag of ${name}`} />
+        </div>
+    )
+}
+
+const CountriesList = (props) => {
+    return (
+        <div>
+            {props.list.map((country, index) => (
+                <div key={index}>{country.name}</div>
+            ))}
+        </div>
+    )
+}
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [nameFilter, setNameFilter] = useState("")
+
+    // grab data
+    const hook = () => {
+        console.log("Grabbing countries...")
+        axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+            console.log("Received!")
+            setCountries(response.data)
+        })
+    }
+    useEffect(hook, [])
+
+    // filter handler
+    const handleFilterChange = (event) => {
+        setNameFilter(event.target.value)
+    }
+
+    // process filtered list
+    const filteredList = countries.filter((country) => {
+        const filter = nameFilter.toLowerCase()
+        const name = country.name.toLowerCase()
+        return name.includes(filter)
+    })
+
+    // determine what to show
+    let display
+    if (nameFilter === "") {
+        // displays nothing if there is no filter
+        display = null
+    } else if (filteredList.length > 10) {
+        display = <SizeError />
+    } else if (filteredList.length === 1) {
+        let country = filteredList[0]
+        display = (
+            <CountryInfo
+                name={country.name}
+                capital={country.capital}
+                population={country.population}
+                languages={country.languages.map((language) => language.name)}
+                flag={country.flag}
+            />
+        )
+    } else {
+        // passing the entire array, not just the country names
+        display = <CountriesList list={filteredList} />
+    }
+
+    return (
+        <div>
+            find countries
+            <input value={nameFilter} onChange={handleFilterChange} />
+            {display}
+        </div>
+    )
+}
+
+export default App
